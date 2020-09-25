7bcdd0c4ffc68ae15697181d9dcb435b5111286a
LanWEB-LLC <71795964+LanWEB-LLC@users.noreply.github.com>
2020-09-25 18:30:55 -0400

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -9,7 +9,13 @@ function Footer() {
 				<div className="footer_welcome">We are ready to help you!</div>
 				<Link className="footer_contact_btn">Contact Us</Link>
 			</div>
-			<div className="footer_announcement">© 2020 LanWEB LLC • All Rights Reserved</div>
+			<div className="footer_row">
+				<Link className="footer_btn">Work Samples</Link>
+			</div>
+			<div className="footer_row">
+				<div className="footer_logo">LW</div>
+			</div>
+			<div className="footer_row">© 2020 LanWEB LLC • All Rights Reserved</div>
 		</div>
 	);
 }
