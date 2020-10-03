e7645b8c92d2c6f3de738ac40b06831e27907b50
LanWEB-LLC <71795964+LanWEB-LLC@users.noreply.github.com>
2020-10-03 19:59:57 -0400

    Add files via upload

---
+++ src/Header_Contact.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Header_Contact.css';
+import { Link } from 'react-router-dom';
+
+function Header_Contact() {
+	return (
+		<nav className="header">
+			<Link to="/" className="without_decoration fadeIn">
+				<div className="header_logo">LW</div>
+			</Link>
+			
+			<div className="header_title fadeIn">
+				<p className="LineOne">LanWEB Software Solution</p>
+				<p className="LineTwo">Creating Impact</p>
+			</div>
+			
+			<div className="header_list">
+				<div className="header_nav">
+					<Link to="/Web" className="header_link fadeIn">
+						<span>Web</span>
+					</Link>
+
+					<Link to="/Gaming" className="header_link fadeIn">
+						<span>Gaming</span>
+					</Link>
+					
+					<Link to="/Contact" className="header_link fadeIn">
+						<span>Contact</span>
+					</Link>
+				</div>
+			</div>
+
+		</nav>
+	)
+}
+
+export default Header_Contact;
