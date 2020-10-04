c6dc686da47ead822d315a6563ab9e4a41f4a02f
LanWEB-LLC <71795964+LanWEB-LLC@users.noreply.github.com>
2020-10-03 20:00:51 -0400

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -5,26 +5,26 @@ import { Link } from 'react-router-dom';
 function Header() {
 	return (
 		<nav className="header">
-			<Link to="/" className="without_decoration">
+			<Link to="/" className="without_decoration fadeIn">
 				<div className="header_logo">LW</div>
 			</Link>
 			
-			<div className="header_title">
+			<div className="header_title fadeIn">
 				<p className="LineOne">LanWEB Software Solution</p>
 				<p className="LineTwo">Creating Impact</p>
 			</div>
 			
 			<div className="header_list">
 				<div className="header_nav">
-					<Link to="/Web" className="header_link">
+					<Link to="/Web" className="header_link fadeIn">
 						<span>Web</span>
 					</Link>
 
-					<Link to="/Gaming" className="header_link">
+					<Link to="/Gaming" className="header_link fadeIn">
 						<span>Gaming</span>
 					</Link>
 					
-					<Link to="/Contact" className="header_link">
+					<Link to="/Contact" className="header_link fadeIn">
 						<span>Contact</span>
 					</Link>
 				</div>
