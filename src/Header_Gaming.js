e2ac48c5d0ef9bfc96d669b05fcf159d50b3bdcf
LanWEB-LLC <71795964+LanWEB-LLC@users.noreply.github.com>
2020-10-03 20:02:23 -0400

    Update Header_Gaming.js

--- src/Header_Gaming.js
+++ src/Header_Gaming.js
@@ -5,26 +5,26 @@ import { Link } from 'react-router-dom';
 function Header_Gaming() {
 	return (
 		<nav className="header">
-			<Link to="/" className="without_decoration">
+			<Link to="/" className="without_decoration fadeIn">
 				<div className="header_logo">LW</div>
 			</Link>
 			
-			<div className="header_title">
+			<div className="header_title fadeIn">
 				<p className="LineOne">Our Gaming Solution</p>
 				<p className="LineTwo">Let's Make Some Fun</p>
 			</div>
 			
 			<div className="header_list">
 				<div className="header_nav">
-					<Link to="/Web" className="header_link">
+					<Link to="/Web" className="header_link fadeIn">
 						<span>Web</span>
 					</Link>
 
-					<Link to="/Gaming" className="header_link">
+					<Link to="/Gaming" className="header_link fadeIn">
 						<span>Gaming</span>
 					</Link>
 					
-					<Link to="/Contact" className="header_link">
+					<Link to="/Contact" className="header_link fadeIn">
 						<span>Contact</span>
 					</Link>
 				</div>
